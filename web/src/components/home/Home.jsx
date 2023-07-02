051685aa03ce13931d82cf8bb7c8f9b656ec3ac9
Mayank Gulati <sagax.dg@gmail.com>
2023-07-03 02:15:52 +0530

    Removed unused imports in Home.jsx

--- web/src/components/home/Home.jsx
+++ web/src/components/home/Home.jsx
@@ -1,15 +1,18 @@
-import { Register } from "./Register";
+// import { Register } from "./Register";
 import { Login } from "./Login";
-import { Box, Button, Card } from "@mui/material";
-import { useState } from "react";
-import { VerifyEmail } from "./VerifyEmail";
+import { Box } from "@mui/material";
+// import { useState } from "react";
+// import { VerifyEmail } from "./VerifyEmail";
 import { useContext } from "react";
 import { UserContext } from "../../context/userContext";
-import { Dashboard } from "../dashboard/Dashboard";
+// import { Dashboard } from "../dashboard/Dashboard";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
 
 const Home = () => {
   const userContext = useContext(UserContext);
   const { userDetails } = userContext;
+  // const navigate = useNavigate();
 
   // console.log(userDetails, userDetails.length);
   return (
