18fe35fb4d0d0b2419a83a3415f72a2ccc3d5e9f
Mayank Gulati <sagax.dg@gmail.com>
2023-07-03 02:22:57 +0530

    Update key.py
    Hard coded the encryption key instead of reading from a file.

--- api/src/key.py
+++ api/src/key.py
@@ -1,5 +1,9 @@
 from cryptography.fernet import Fernet
 
-with open(r"./../assets/key", "r") as f:
-    key = f.read()
-    fernet = Fernet(key)
+#! unable to read file when running pytest
+# with open(r"./../assets/key", "r") as f:
+#     key = f.read()
+#     fernet = Fernet(key)
+
+key = "B4i3n_UKh-pKijF03VbuuzWg5oQK5IfdSnGVMIsNzVg="
+fernet = Fernet(key)
